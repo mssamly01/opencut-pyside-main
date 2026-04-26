782f8ec6801ae58e198c32db52018999baab2572
Ralph Littel <42280371+mssamly01@users.noreply.github.com>
2026-04-27 00:01:48 +0700

    Delete reference/opencut directory

--- reference/opencut/apps/web/src/lib/wasm/ticks.ts
+++
@@ -1,3 +0,0 @@
-import { TICKS_PER_SECOND as _TICKS_PER_SECOND } from "opencut-wasm";
-
-export const TICKS_PER_SECOND = _TICKS_PER_SECOND();
--- reference/opencut/apps/web/src/services/storage/migrations/base.ts
+++
@@ -1,9 +0,0 @@
-import type { MigrationResult, ProjectRecord } from "./transformers/types";
-
-export abstract class StorageMigration {
-	abstract from: number;
-	abstract to: number;
-	abstract transform(
-		project: ProjectRecord,
-	): Promise<MigrationResult<ProjectRecord>>;
-}
